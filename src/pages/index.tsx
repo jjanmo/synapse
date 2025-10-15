d32cbb54497aaa3a958afb4bbd53286082cb7155
jjanmo <jjanmo07@hanmail.net>
2025-10-16 01:21:17 +0900

    refactor: 새로운 홈페이지 추가(기존 홈은 intro로 변경)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,19 @@
-import { useState, type FC } from 'react';
-import styles from '@/styles/pages/home.module.css';
-import type { Nullable } from '@/types/common';
-import { useWebApiListQuery } from '@/queries/scrape';
-import Loading from '@/components/common/Loading';
-import WebApiModal from '@/components/home/WebApiModal';
-import Introduction from '@/components/home/Introduction';
+import { WEB_API_LIST } from '@/constants/home';
+import Link from 'next/link';
 
-const SCRAPE_URL = 'https://developer.mozilla.org/ko/docs/Web/API';
-
-const Home: FC = () => {
-  const { data: groupedWebApis, isPending } = useWebApiListQuery(SCRAPE_URL);
-  const [selectedWebApiKey, setSelectedWebApiKey] = useState<Nullable<string>>(null);
-
-  const handleCloseModal = () => {
-    setSelectedWebApiKey(null);
-  };
-
-  const handleWebApiClick = (id: string) => () => {
-    setSelectedWebApiKey(id);
-  };
-
+const Home = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <Introduction />
-        <ul className={styles.list}>
-          {isPending && <Loading overrideStyle={styles.loading} />}
-          {groupedWebApis &&
-            Object.entries(groupedWebApis).map(([firstLetter, webApiItems]) => {
-              const firstWebApiText = webApiItems[0].title;
-              const count = webApiItems.length;
+    <ul>
+      {WEB_API_LIST.map((webApi) => {
+        const { slug, title } = webApi;
 
-              return (
-                <li className={styles.item} key={firstLetter} onClick={handleWebApiClick(firstLetter)}>
-                  <div className={styles.firstLetter}>{firstLetter}</div>
-                  <div className={styles.firstItemText}>
-                    <span>{firstWebApiText}</span>
-                    {count > 1 && ` 외 ${count - 1}개`}
-                  </div>
-                </li>
-              );
-            })}
-        </ul>
-      </div>
-      {groupedWebApis && selectedWebApiKey && (
-        <WebApiModal
-          webApiList={groupedWebApis[selectedWebApiKey]}
-          isOpen={!!selectedWebApiKey}
-          title={selectedWebApiKey}
-          onCloseModal={handleCloseModal}
-        />
-      )}
-    </main>
+        return (
+          <li key={slug}>
+            <Link href={`/web-apis/${slug}`}>{title}</Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 };
 
---
+++ src/pages/intro.tsx
@@ -0,0 +1,58 @@
+import { useState, type FC } from 'react';
+import styles from '@/styles/pages/intro.module.css';
+import type { Nullable } from '@/types/common';
+import { useWebApiListQuery } from '@/queries/scrape';
+import Loading from '@/components/common/Loading';
+import WebApiModal from '@/components/home/WebApiModal';
+import Introduction from '@/components/home/Introduction';
+
+const SCRAPE_URL = 'https://developer.mozilla.org/ko/docs/Web/API';
+
+const Home: FC = () => {
+  const { data: groupedWebApis, isPending } = useWebApiListQuery(SCRAPE_URL);
+  const [selectedWebApiKey, setSelectedWebApiKey] = useState<Nullable<string>>(null);
+
+  const handleCloseModal = () => {
+    setSelectedWebApiKey(null);
+  };
+
+  const handleWebApiClick = (id: string) => () => {
+    setSelectedWebApiKey(id);
+  };
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Introduction />
+        <ul className={styles.list}>
+          {isPending && <Loading overrideStyle={styles.loading} />}
+          {groupedWebApis &&
+            Object.entries(groupedWebApis).map(([firstLetter, webApiItems]) => {
+              const firstWebApiText = webApiItems[0].title;
+              const count = webApiItems.length;
+
+              return (
+                <li className={styles.item} key={firstLetter} onClick={handleWebApiClick(firstLetter)}>
+                  <div className={styles.firstLetter}>{firstLetter}</div>
+                  <div className={styles.firstItemText}>
+                    <span>{firstWebApiText}</span>
+                    {count > 1 && ` 외 ${count - 1}개`}
+                  </div>
+                </li>
+              );
+            })}
+        </ul>
+      </div>
+      {groupedWebApis && selectedWebApiKey && (
+        <WebApiModal
+          webApiList={groupedWebApis[selectedWebApiKey]}
+          isOpen={!!selectedWebApiKey}
+          title={selectedWebApiKey}
+          onCloseModal={handleCloseModal}
+        />
+      )}
+    </main>
+  );
+};
+
+export default Home;
